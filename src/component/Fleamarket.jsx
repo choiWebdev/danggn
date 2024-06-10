c4dbf2888d2768c55b3fa49650154406e23f730a
choiWebdev <choi.webdev@gmail.com>
2024-06-10 16:45:00 +0900

    add sub top code

---
+++ src/component/Fleamarket.jsx
@@ -0,0 +1,13 @@
+import './Fleamarket.css';
+import SubTop from './SubTop';
+
+export default function Fleamarket() {
+  return (
+    <>
+      <SubTop />
+      <main>
+        abc
+      </main>
+    </>
+  )
+};
--- src/component/MainPage.jsx
+++ src/component/MainPage.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import googlePlayImg from './../img/icon_gooleplay.svg';
 import appStoreImg from './../img/icon_appstore.svg';
 import mainSayHiImg from './../img/main_sayHi.png';
@@ -12,21 +12,19 @@ export default function MainPage() {
   return (
     <main>
       <section className="main_section main_section_download">
-        <div className="inner">
+        <div className="inner1024">
           <div className="txt_box">
             <h2 className="big_title">당신 근처의 <br />지역 생활 커뮤니티</h2>
             <p className="section_txt">동네라서 가능한 모든 것<br />당근에서 가까운 이웃과 함께해요.</p>
             <div className="btn_wrap">
-              <BrowserRouter>
-                <Link className="btn_style_carrot btn_download" to="">
-                  <img src={googlePlayImg} alt="구글 플레이에서 다운로드" />
-                  Google Play
-                </Link>
-                <Link className="btn_style_carrot btn_download" to="">
-                  <img src={appStoreImg} alt="앱 스토어에서 다운로드" />
-                  App Store
-                </Link>
-              </BrowserRouter>
+              <Link className="btn_style_carrot btn_download" to="https://play.google.com/store/apps/details?id=com.towneers.www" target="_blank">
+                <img src={googlePlayImg} alt="구글 플레이에서 다운로드" />
+                Google Play
+              </Link>
+              <Link className="btn_style_carrot btn_download" to="https://apps.apple.com/app/id1018769995" target="_blank">
+                <img src={appStoreImg} alt="앱 스토어에서 다운로드" />
+                App Store
+              </Link>
             </div>
           </div>
           <div className="img_box">
@@ -35,16 +33,14 @@ export default function MainPage() {
         </div>
       </section>
       <section className="main_section main_section_left main_section01">
-        <div className="inner">
+        <div className="inner1024">
           <div className="txt_box">
             <p className="small_title">중고거래</p>
             <h2 className="big_title">믿을만한<br />이웃 중고거래</h2>
             <p className="section_txt">동네 주민들과 가깝고 따뜻한 거래를 <br />지금 경험해보세요</p>
             <div className="btn_wrap">
-              <BrowserRouter>
-                <Link className="btn_style_graybox" to="">인기매물 보기</Link>
-                <Link className="btn_style_graybox" to="">믿을 수 있는 중고거래</Link>
-              </BrowserRouter>
+              <Link className="btn_style_graybox" to="">인기매물 보기</Link>
+              <Link className="btn_style_graybox" to="">믿을 수 있는 중고거래</Link>
             </div>
           </div>
           <div className="img_box">
@@ -53,7 +49,7 @@ export default function MainPage() {
         </div>
       </section>
       <section className="main_section main_section_right main_section02">
-        <div className="inner">
+        <div className="inner1024">
           <div className="txt_box">
             <p className="small_title">동네생활</p>
             <h2 className="big_title">이웃만 아는<br/>동네 정보와 이야기</h2>
@@ -82,15 +78,13 @@ export default function MainPage() {
         </div>
       </section>
       <section className="main_section main_section_left main_section03">
-        <div className="inner">
+        <div className="inner1024">
           <div className="txt_box">
             <p className="small_title">알바</p>
             <h2 className="big_title">걸어서 10분!<br/>동네 알바 구하기</h2>
             <p className="section_txt">당근하듯 쉽고, 편하게<br/> 당근 알바로 동네 알바를 구할 수 있어요.</p>
             <div className="btn_wrap">
-              <BrowserRouter>
-                <Link className="btn_style_graybox" to="">내근처 알바 보기</Link>
-              </BrowserRouter>
+              <Link className="btn_style_graybox" to="">내근처 알바 보기</Link>
             </div>
           </div>
           <div className="img_box">
@@ -99,15 +93,13 @@ export default function MainPage() {
         </div>
       </section>
       <section className="main_section main_section_right main_section04">
-        <div className="inner">
+        <div className="inner1024">
           <div className="txt_box">
             <p className="small_title">동네업체</p>
             <h2 className="big_title">내 근처에서 찾는<br/>동네업체</h2>
             <p className="section_txt">이웃들의 추천 후기를 확인하고<br/>동네 곳곳의 업체들을 찾을 수 있어요.</p>
             <div className="btn_wrap">
-              <BrowserRouter>
-                <Link className="btn_style_graybox" to="">당근 동네업체 보기</Link>
-              </BrowserRouter>
+              <Link className="btn_style_graybox" to="">당근 동네업체 보기</Link>
             </div>
           </div>
           <div className="img_box">
